1c22e2fb27132890165ef58eadcaa0c34a5faa5d
AndresMartinMaq <andrewtri800@hotmail.com>
2017-09-06 13:32:49 +0200

    Added socket.io and simple example using listening to onConnect.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 //We use path instead of simlply __dirname + /../public because it joins
 //paths correctly. Otherwise, we'd get a weird /server/../public path which might cause problems later
@@ -7,10 +9,20 @@ const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+//The following two lines are needed for SocketIO.
+const server = http.createServer( app );
+const io = socketIO(server);
+
+//Listen to event when a client connects
+io.on('connection', (socket) => {
+	console.log('New User connected-');
+
+	socket.on('disconnect', () => console.log('Client Disconnected'));
+});
 
 //Serve the public folder
 app.use(express.static(publicPath));
 
 
 //Begin Serving
-app.listen(PORT, () => console.log('Started serving at port '+PORT));
+server.listen(PORT, () => console.log('Started serving at port '+PORT));
